10964a50e04949baef0aa06365b1448dbdf4724a
Sudhanshu Mishra <115578381+Sudhanshu-m@users.noreply.github.com>
2026-01-07 18:55:03 +0000

    data type done

---
+++ JS_basics/data_types.js
@@ -0,0 +1,20 @@
+"use strict"; // used to treat all JS code as newer version
+
+// alert (3+3) // we are using nodeJS not browser 
+
+let name = "Sudhanshu" // this is string data type
+let age = 21 // this is integer data type
+let isloggedIN = true // this is boolean data type
+
+// types of Data types are below
+// number => any number
+// bigint => used when the integer is too large
+// string => to represent normal sentences
+// boolean => true/ false or in simple words yes or no 
+// NULL => it is representation of empty variabel 
+// Undefined => means value not yet assigned 
+// symbole => to define unique components 
+
+// typeof tells the data type of the variable
+console.log(typeof undefined); // output will be undefined as it is a data type
+console.log(typeof null); // output of this will be object
